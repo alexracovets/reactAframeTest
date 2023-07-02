c22898cf0aa8196388f2b11fdc237069a5c65c78
Aleksandr Racovets <57405165+alexracovets@users.noreply.github.com>
2023-07-02 23:19:11 +0300

    fixed test

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
-import { useEffect } from "react";
+import AppScene from "./Components/ARScene/ARScene";
 
 function App() {
-    useEffect(() => {
-        if (navigator.geolocation) {
-            const arObject = document.getElementById("arObject");
-
-            navigator.geolocation.getCurrentPosition(function (position) {
-                const latitude = position.coords.latitude;
-                const longitude = position.coords.longitude;
-
-                if (arObject) {
-                    arObject.setAttribute("gps-entity-place", `latitude: ${latitude}; longitude: ${longitude};`);
-                }
-            });
-
-        } else {
-            console.log('Геолокація не підтримується вашим браузером');
-        }
-    });
-
-
     return (
-        <a-scene embedded arjs="trackingMethod: best; debugUIEnabled: false;">
-            <a-camera gps-camera rotation-reader></a-camera>
-            <a-entity id="arObject" geometry="primitive: box; depth: 1; height: 1; width: 1" material="color: red"
-                gps-entity-place="latitude: 50.452505227936946; longitude: 30.354042820289482;" />
-        </a-scene>
+        <AppScene />
     );
 }
 
-export default App;
+export default App; 
--- src/Components/ARScene/ARScene.jsx
+++ src/Components/ARScene/ARScene.jsx
@@ -1,9 +1,19 @@
-function ARScene() {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+import 'aframe';
+import React from 'react';
+import { Scene, Entity } from 'react-aframe-ar';
 
-export default ARScene;
+export default class AppScene extends React.Component {
+    render() {
+        return (
+            <Scene>
+                <Entity gps-entity-place="latitude: 50.452505227936946; longitude: 30.354042820289482;">
+                    <Entity
+                        geometry={{ primitive: 'box', width: 1 }}
+                        position="0 1 -5"
+                        material="color: red"
+                    />
+                </Entity>
+            </Scene>
+        );
+    }
+}
