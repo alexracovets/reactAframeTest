fcc478e8c47dab1ee2bbc4904064c2135cd5a6a0
Aleksandr Racovets <57405165+alexracovets@users.noreply.github.com>
2023-07-02 23:28:16 +0300

    fixed test

--- src/Components/ARScene/ARScene.jsx
+++ src/Components/ARScene/ARScene.jsx
@@ -4,15 +4,19 @@ import { Scene, Entity } from 'react-aframe-ar';
 
 export default class AppScene extends React.Component {
     render() {
+        const latitude = 50.452505227936946;
+        const longitude = 30.354042820289482;
+
         return (
             <Scene>
-                <Entity gps-entity-place="latitude: 50.452505227936946; longitude: 30.354042820289482;">
+                <Entity gps-entity-place={`latitude: ${latitude}; longitude: ${longitude};`}>
                     <Entity
                         geometry={{ primitive: 'box', width: 1 }}
                         position="0 1 -5"
                         material="color: red"
                     />
                 </Entity>
+                <Entity gps-camera rotation-reader />
             </Scene>
         );
     }
