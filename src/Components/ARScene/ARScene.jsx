dd45f112ac38bba84b59db4a72f8102f89ca4237
Aleksandr Racovets <57405165+alexracovets@users.noreply.github.com>
2023-07-03 01:35:21 +0300

    fixed test

--- src/Components/ARScene/ARScene.jsx
+++ src/Components/ARScene/ARScene.jsx
@@ -1,27 +1,8 @@
-import 'aframe';
-import React, { useEffect, useState, useRef } from 'react';
+import 'aframe'; 
 import { Scene, Entity, Box } from 'react-aframe-ar';
 
 export default function AppScene() {
-    const [arObjectAttributes, setArObjectAttributes] = useState({ longitude: 30.354042820289482, latitude: 50.452505227936946 });
-    const arObjectRef = useRef(null);
-    useEffect(() => {
-        const watchPositionId = navigator.geolocation.watchPosition(
-            position => {
-                const { latitude, longitude } = position.coords;
-                setArObjectAttributes({ longitude, latitude });
-                console.log('Geolocation:', position.coords);
-            },
-            error => {
-                console.error('Error retrieving geolocation:', error);
-            }
-        );
-
-        return () => {
-            navigator.geolocation.clearWatch(watchPositionId);
-        };
-    }, []);
-
+ 
     return (
         <Scene
             vr-mode-ui="enabled: false"
@@ -29,10 +10,9 @@ export default function AppScene() {
             arjs="sourceType: webcam; debugUIEnabled: false;"
         >
             <Box
-                id="arObject"
-                ref={arObjectRef}
+                id="arObject" 
                 material="color: red"
-                gps-entity-place={`longitude: ${arObjectAttributes.longitude - 0.0005}; latitude: ${arObjectAttributes.latitude + 0.0006};`}
+                gps-entity-place={`longitude: 30.354042820289482; latitude: 50.452505227936946;`}
             />
 
             <Entity gps-camera rotation-reader />
