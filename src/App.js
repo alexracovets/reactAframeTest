b1a283561620fe9c9cdc707fa9221799cb2781f4
Aleksandr Racovets <57405165+alexracovets@users.noreply.github.com>
2023-07-02 20:42:12 +0300

    fixed test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
+import { useEffect } from "react";
+
 function App() {
+    useEffect(() => {
+        if (navigator.geolocation) {
+            const arObject = document.getElementById("arObject");
+
+            navigator.geolocation.getCurrentPosition(function (position) {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+
+                if (arObject) {
+                    arObject.setAttribute("gps-entity-place", `latitude: ${latitude}; longitude: ${longitude};`);
+                }
+            });
+
+        } else {
+            console.log('Геолокація не підтримується вашим браузером');
+        }
+    });
+
+
     return (
         <a-scene embedded arjs="trackingMethod: best; debugUIEnabled: false;">
             <a-camera gps-camera rotation-reader></a-camera>
