c6f35e8117d7d6b7c9681274516a0ce1fa7d64b8
Aleksandr Racovets <57405165+alexracovets@users.noreply.github.com>
2023-07-03 04:38:20 +0300

    fixed test

--- src/Components/ARScene/ARScene.jsx
+++ src/Components/ARScene/ARScene.jsx
@@ -1,40 +1,10 @@
-import 'aframe';
-import React, { useEffect } from 'react';
-import { Scene, Entity, Box } from 'react-aframe-ar';
-
+ 
 export default function AppScene() {
-    const arObject = React.useRef(null);
-    const ArScene = React.useRef(null);
-    useEffect(() => {
-        const watchPositionId = navigator.geolocation.watchPosition(
-            position => {
-                console.log(position.coords)
-                console.log(arObject)
-            },
-            error => {
-                console.error('Error retrieving geolocation:', error);
-            }
-        );
-
-        return () => {
-            navigator.geolocation.clearWatch(watchPositionId);
-        };
-    }, []);
-
     return (
-        <Scene ref={ArScene}
-            vr-mode-ui="enabled: false"
-            embedded
-            arjs="sourceType: webcam; debugUIEnabled: false;"
-        >
-            <Box
-                id="arObject"
-                ref={arObject}
-                material="color: red"
-                gps-entity-place="latitude: 50.452481; longitude: 30.354043;"
-            />
-
-            <Entity gps-camera rotation-reader />
-        </Scene>
+        <a-scene embedded arjs="trackingMethod: best; debugUIEnabled: false;">
+            <a-camera gps-camera rotation-reader position="0 1 3"></a-camera>
+            <a-entity id="arObject" geometry="primitive: box; depth: 1; height: 1; width: 1" material="color: red"
+                gps-entity-place="latitude: 50.452481; longitude: 30.354043;" />
+        </a-scene>
     );
 }
