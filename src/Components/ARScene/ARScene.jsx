1f24e6ed60a9c572319ea03b1b29ddcb963900f5
Aleksandr Racovets <57405165+alexracovets@users.noreply.github.com>
2023-07-03 04:30:59 +0300

    fixed test

--- src/Components/ARScene/ARScene.jsx
+++ src/Components/ARScene/ARScene.jsx
@@ -3,10 +3,13 @@ import React, { useEffect } from 'react';
 import { Scene, Entity, Box } from 'react-aframe-ar';
 
 export default function AppScene() {
+    const arObject = React.useRef(null);
+    const ArScene = React.useRef(null);
     useEffect(() => {
         const watchPositionId = navigator.geolocation.watchPosition(
             position => {
                 console.log(position.coords)
+                console.log(arObject)
             },
             error => {
                 console.error('Error retrieving geolocation:', error);
@@ -19,13 +22,14 @@ export default function AppScene() {
     }, []);
 
     return (
-        <Scene
+        <Scene ref={ArScene}
             vr-mode-ui="enabled: false"
             embedded
             arjs="sourceType: webcam; debugUIEnabled: false;"
         >
             <Box
                 id="arObject"
+                ref={arObject}
                 material="color: red"
                 gps-entity-place="latitude: 50.452481; longitude: 30.354043;"
             />
